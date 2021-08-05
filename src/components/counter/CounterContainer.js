a9eb32d01f55bd10b7beb858163d9c3c0188a18b
dhbalaji <pessebalu@gmail.com>
2021-08-05 19:32:55 +0530

    Updated to latest

--- src/components/counter/CounterContainer.js
+++ src/components/counter/CounterContainer.js
@@ -1,24 +1,46 @@
-import { connect } from 'react-redux'
-import Counter from './Counter'
-import { bindActionCreators } from 'redux'
+import { Link } from 'react-router-dom'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import { incrementAction, decrementAction, clearAction } from './actions'
 import { getCount, shouldShowToast } from './selector'
-function mapStateToProps(state) {
-    return {
-        count: getCount(state),
-        showError: shouldShowToast(state)
-    }
-}
+import React, { useCallback, useEffect } from 'react'
+import counterCss from './Counter.module.scss'
 
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators(
-        {
-            incrementAction,
-            decrementAction,
-            clearAction
-        },
-        dispatch
+const Counter = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        return () => {
+            dispatch(clearAction())
+        }
+    }, [dispatch])
+
+    const showError = useSelector(shouldShowToast)
+    const count = useSelector(getCount)
+    const increaseCounter = useCallback(() => dispatch(incrementAction()), [dispatch])
+    const decreaseCounter = useCallback(() => dispatch(decrementAction()), [dispatch])
+    return (
+        <div className={counterCss.counter}>
+            <h2>Simple Counter</h2>
+            {showError && (
+                <div className={counterCss.counter__err__txt}>
+                    Max 100 allowed
+                </div>
+            )}
+            <div>
+                <button
+                    onClick={decreaseCounter}
+                    className={counterCss.button}
+                    disabled={count <= 0}
+                >
+                    -
+                </button>
+                <span className={counterCss.counter__text}>{count}</span>
+                <button onClick={increaseCounter}>+</button>
+            </div>
+
+            <Link to='/'>Home</Link>
+        </div>
     )
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+
+export default connect()(Counter)
